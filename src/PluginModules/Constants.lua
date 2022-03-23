0fb838b616119142e8d9cc537a5791b3c62e72c3
Blupo <blupo@protonmail.com>
2022-03-23 13:07:37 -0700

    Calculate max keypoints

--- src/PluginModules/Constants.lua
+++ src/PluginModules/Constants.lua
@@ -1,8 +1,33 @@
-return {
-    MAX_COLORSEQUENCE_KEYPOINTS = 20,
+local CONSTANTS = {
     VALID_GRADIENT_COLOR_SPACES = { "RGB", "CMYK", "HSB", "HWB", "HSL", "Lab", "Luv", "LChab", "LChuv", "xyY", "XYZ" },
     VALID_HUE_ADJUSTMENTS = { "Shorter", "Longer", "Increasing", "Decreasing", "Raw" },
 
     KELVIN_LOWER_RANGE = 1000,
     KELVIN_UPPER_RANGE = 10000,
 }
+
+do
+    local n: number = 2
+    local csConstructionOk: boolean = true
+    
+    -- in case the limit is removed, cap at 100
+    while ((csConstructionOk) and (n < 101)) do
+        n = n + 1
+
+        local keypoints: {ColorSequenceKeypoint} = {}
+
+        for i = 1, n do
+            table.insert(keypoints, ColorSequenceKeypoint.new((i - 1) / (n - 1), Color3.new()))
+        end
+
+        csConstructionOk = pcall(function()
+            ColorSequence.new(keypoints)
+        end)
+    end
+
+    CONSTANTS.MAX_COLORSEQUENCE_KEYPOINTS = n - 1
+end
+
+---
+
+return CONSTANTS
